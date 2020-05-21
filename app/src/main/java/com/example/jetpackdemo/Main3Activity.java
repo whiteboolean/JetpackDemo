1620c6fac91bab218bee9568bacd9f22910e7236
booleanwhite <booleanwhite@gmail.com>
2020-05-21 15:25:43 +0800

    提交liveData相关操作

---
+++ app/src/main/java/com/example/jetpackdemo/Main3Activity.java
@@ -0,0 +1,47 @@
+package com.example.jetpackdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main3Activity extends AppCompatActivity {
+    MyViewModel myViewModel;
+    ViewModelProvider.AndroidViewModelFactory factory ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        factory = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication());
+        myViewModel = factory.create(MyViewModel.class);
+        MutableLiveData<Integer> likedNumber = myViewModel.getLikedNumber();
+        final TextView textView = findViewById(R.id.textView5);
+        likedNumber.observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                textView.setText(String.valueOf(integer));
+            }
+        });
+
+        findViewById(R.id.button11).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              myViewModel.setLikedNumber(1);
+            }
+        });
+
+        findViewById(R.id.button12).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            myViewModel.setLikedNumber(-1);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/jetpackdemo/MyViewModel.java
@@ -0,0 +1,20 @@
+package com.example.jetpackdemo;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+
+    private MutableLiveData<Integer> likedNumber;
+    public MutableLiveData<Integer> getLikedNumber(){
+        if (likedNumber==null){
+            likedNumber = new MutableLiveData<>();
+            likedNumber.setValue(0);
+        }
+        return likedNumber;
+    }
+
+    public void setLikedNumber(int number){
+        likedNumber.setValue(likedNumber.getValue()-number);
+    }
+}
