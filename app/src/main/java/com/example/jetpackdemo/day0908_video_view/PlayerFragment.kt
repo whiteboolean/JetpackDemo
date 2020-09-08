c2c2700aa461c7456aa3902daed50ad7e200be66
jett <jett@dafacloud.com>
2020-09-08 10:43:59 +0800

    仿抖音 实现viewpager2嵌套

---
+++ app/src/main/java/com/example/jetpackdemo/day0908_video_view/PlayerFragment.kt
@@ -0,0 +1,112 @@
+package com.example.jetpackdemo.day0908_video_view
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.SurfaceHolder
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.example.jetpackdemo.R
+import kotlinx.android.synthetic.main.fragment_player.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [PlayerFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class PlayerFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private val mediaPlayer = MediaPlayer()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_player, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mediaPlayer.apply {
+            setOnPreparedListener{
+//                it.start()
+                seekTo(1)
+                progressBar4.max = mediaPlayer.duration
+                progressBar2.visibility = View.INVISIBLE
+            }
+            setDataSource(param1)
+            prepareAsync()
+            progressBar2.visibility = View.VISIBLE
+        }
+
+        lifecycleScope.launch {
+            while (true){
+                progressBar4.progress  = mediaPlayer.currentPosition
+                delay(500)
+            }
+        }
+        surfaceView2.holder.addCallback(object:SurfaceHolder.Callback{
+            override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+                mediaPlayer.setDisplay(holder)
+                mediaPlayer.setScreenOnWhilePlaying(true)
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder?) {}
+
+            override fun surfaceCreated(holder: SurfaceHolder?) {
+
+            }
+        })
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mediaPlayer.start()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer.pause()
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment PlayerFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+                PlayerFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
+                }
+    }
+}
