c0821c8b96f95cf4002b0a1b9b2523ddf63fa471
whiteboolean@gmail.com <github19930518gd>
2021-02-04 19:27:32 +0800

    1

--- app/src/main/java/com/example/jetpackdemo/MyActivity3.java
+++ app/src/main/java/com/example/jetpackdemo/MyActivity3.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.junit.Test;
+
 public class MyActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my3);
+    }
+
+
+    @Test
+    public void test1(){
+        System.out.println("测试方法");
     }
 }
