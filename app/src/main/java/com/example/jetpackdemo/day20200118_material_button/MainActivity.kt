c60cafadcb92f2506ceefcf8e389dbec6e3e9269
guandi <guandi@archermind.com>
2022-01-21 15:20:51 +0800

    flexBoxLayout相关用法

---
+++ app/src/main/java/com/example/jetpackdemo/day20200118_material_button/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.jetpackdemo.day20200118_material_button
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jetpackdemo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main34)
+
+
+    }
+}
